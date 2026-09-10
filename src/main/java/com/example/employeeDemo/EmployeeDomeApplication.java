b1abb12f47a2a2d1bd0502498b21db814b6f634f
Prashan070 <prashansrivastava77@gmail.com>
2026-09-10 23:45:23 +0530

    Initial commit

---
+++ src/main/java/com/example/employeeDemo/EmployeeDomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDomeApplication.class, args);
+	}
+
+}
